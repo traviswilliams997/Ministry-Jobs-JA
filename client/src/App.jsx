a81ee53216c458f5a5a64806a3f434124ffb3e6e
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-03-06 03:47:14 -0500

    Create JobCard Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Stack, useMediaQuery } from '@mui/material'
 import Navbar from './components/Navbar'
+import MohCard from './components/MohCard'
 function App() {
+  const isNonMobile = useMediaQuery('(min-width:1200px)')
+
   return (
-    <Box bgcolor={'#77A6F7'} color={'#FFFFFF'}>
+    <Stack>
       <Navbar />
-    </Box>
+      <Box display={'flex'} justifyContent={'right'}>
+        <Box
+          display="grid"
+          gap="30px"
+          width="50vw"
+          pt={'40px'}
+          bgcolor="#333A56"
+          justifyItems={'center'}
+          gridTemplateColumns="repeat(1, minmax(0, 1fr))"
+        >
+          <MohCard />
+          <MohCard />
+          <MohCard />
+          <MohCard />
+          <MohCard />
+          <MohCard />
+        </Box>
+      </Box>
+    </Stack>
   )
 }
 
